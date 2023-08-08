1fb16bcb23605746a16c822f286a4a5a9b06370c
flappy132 <dimitriev01@list.ru>
2023-08-08 17:40:49 +0700

    Add eslint and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      jsxRuntime: 'classic'
+    }), viteTsconfigPaths(), svgrPlugin(),
+    VitePWA({
+      manifest: {
+        name: 'Market App',
+        short_name: 'Market',
+        display: 'standalone',
+      }
+    }),
+  ],
+  build: {
+    outDir: 'build',
+    assetsDir: 'static',
+
+    rollupOptions: {
+      output: {
+        assetFileNames: (fileInfo) => {
+          if (fileInfo.name.endsWith('.css')) {
+            return 'static/css/[name].[hash][extname]';
+          }
+          else if (/\.(svg|png|jpe?g|gif|webp)$/i.test(fileInfo.name)) {
+            return 'static/img/[name].[hash][extname]';
+          }
+          else if (/\.(js)(\?.*)?$/.test(fileInfo.name)) {
+            return 'static/js/[name].[hash][extname]';
+          }
+          else {
+            return 'static/[name].[hash][extname]';
+          }
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+});
